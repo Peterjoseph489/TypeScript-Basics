77d0cdbc5e8284eff0151c2ac2daa5ea3d5c946a
Joseph <josephpeter489@gmail.com>
2023-09-23 14:02:17 +0100

    let us learn

--- intro.ts
+++ intro.ts
@@ -3,4 +3,15 @@ const user = { name: "Joseph", age: 10 };
 console.log("Joseph");
 
 console.log(user.name);
+
+// Functions
+function addTwo (num: number): number {
+    return 7
+    // return 'Hello
+}
+
+let myValue = addTwo(5)
+console.log(myValue)
+
+
 export {};
