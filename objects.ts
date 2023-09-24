b74d9fafbab158d39b0cb2db917af270d4dbb5db
Joseph <josephpeter489@gmail.com>
2023-09-24 12:56:09 +0100

    let us learn

---
+++ objects.ts
@@ -0,0 +1,12 @@
+const user = {
+    name: 'Joseph',
+    email: "joseph@gmil.com",
+    isActive: true
+};
+
+function createUser ({ name: string, isPaid: boolean }) {}
+
+createUser({ name: 'Joseph', isPaid: false }); 
+
+
+export {}
